3dd5be97cf189c0dd422120745e17967ccb01022
Kacper Bąk <kacper.bak@sonderfin.com>
2022-11-01 22:30:31 +0100

    feat: add user to DB

--- app/src/main/java/com/example/exchangeabletoken/DataBaseService.kt
+++ app/src/main/java/com/example/exchangeabletoken/DataBaseService.kt
@@ -4,6 +4,16 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class DataBaseService {
+    fun addUser(uid: String, name: String, email: String, phone: String, address: String) {
+        val database = Firebase.database
+        val myRef = database.getReference("users")
+        myRef.child(name).child("name").setValue(name)
+        myRef.child(name).child("uid").setValue(uid)
+        myRef.child(name).child("email").setValue(email)
+        myRef.child(name).child("phone").setValue(phone)
+        myRef.child(name).child("address").setValue(address)
+    }
+
     companion object {
         fun createProduct() {
             FirebaseDatabase.getMarketData().forEach {
