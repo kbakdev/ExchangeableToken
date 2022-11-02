7a39aea7aff6aea0312b4dfb2aae01bf4677cae1
Kacper Bąk <kacper.bak@sonderfin.com>
2022-11-02 21:55:41 +0100

    feat: product adapter

--- app/src/main/java/com/example/exchangeabletoken/ProductAdapter.kt
+++ app/src/main/java/com/example/exchangeabletoken/ProductAdapter.kt
@@ -4,8 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductAdapter(productsByCategory: List<DataProduct>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private val productsByCategory: List<DataProduct> = productsByCategory
+class ProductAdapter(private val productsByCategory: List<DataProduct>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
         return ProductViewHolder(view)
--- app/src/main/java/com/example/exchangeabletoken/ProductViewHolder.kt
+++ app/src/main/java/com/example/exchangeabletoken/ProductViewHolder.kt
@@ -3,8 +3,9 @@ package com.example.exchangeabletoken
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductViewHolder(view: View?) : RecyclerView.ViewHolder(view!!) {
-    fun bind(product: DataProduct) {
-        TODO("Not yet implemented")
+class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(product: DataProduct): DataProduct {
+        return product
     }
+
 }
