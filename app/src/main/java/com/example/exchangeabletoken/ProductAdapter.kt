3a713285708aada3461b567c0b9821aaf28bedc0
Kacper Bąk <kacper.bak@sonderfin.com>
2022-11-02 21:44:28 +0100

    feat: product category implement datasearch

---
+++ app/src/main/java/com/example/exchangeabletoken/ProductAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.exchangeabletoken
+
+class ProductAdapter(productsByCategory: Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+}
---
+++ app/src/main/java/com/example/exchangeabletoken/ProductViewHolder.kt
@@ -0,0 +1,5 @@
+package com.example.exchangeabletoken
+
+class ProductViewHolder(view: View?) : RecyclerView.ViewHolder() {
+
+}
