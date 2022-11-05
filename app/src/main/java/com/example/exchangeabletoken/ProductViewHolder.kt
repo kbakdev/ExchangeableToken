b09aa98e606883b2d521ae62cb16f6e811324502
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2022-11-05 21:55:55 +0100

    Product search category in UI (#65)
    * feat: firebase support
    
    * feat: idea
    
    * feat: merge
    
    * feat: add the firebase authentication SDK
    
    * feat: added integration with firebase and sign out
    
    * feat: added login to firebase, and connection with login provider
    
    * feat: logIn and market
    
    * feat: market and login
    
    * feat: login
    
    * feat(UI): added search by category query

--- app/src/main/java/com/example/exchangeabletoken/ProductViewHolder.kt
+++ app/src/main/java/com/example/exchangeabletoken/ProductViewHolder.kt
@@ -1,11 +1,12 @@
 package com.example.exchangeabletoken
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(product: DataProduct): DataProduct {
-        return product
+        val textView: TextView = itemView.findViewById(R.id.textView)
+        return DataProduct(product.id, product.name, product.price, product.image, product.category)
     }
-
 }
