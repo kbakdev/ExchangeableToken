f63f1c58ad608497fc806d421437637fbe0680af
Kacper Bąk <kacper.bak@sonderfin.com>
2022-11-27 20:19:55 +0100

    feat(data): random dataProduct generator

--- app/src/main/java/com/example/exchangeabletoken/DataBaseService.kt
+++ app/src/main/java/com/example/exchangeabletoken/DataBaseService.kt
@@ -8,11 +8,11 @@ class DataBaseService {
     fun addUser(uid: String, name: String, email: String, phone: String, address: String) {
         val database = Firebase.database
         val myRef = database.getReference("users")
-        myRef.child(name).child("name").setValue(name)
+        myRef.child(name).child("name").setValue(name.lowercase())
         myRef.child(name).child("uid").setValue(uid)
-        myRef.child(name).child("email").setValue(email)
+        myRef.child(name).child("email").setValue(email.lowercase())
         myRef.child(name).child("phone").setValue(phone)
-        myRef.child(name).child("address").setValue(address)
+        myRef.child(name).child("address").setValue(address.lowercase())
     }
 
     companion object {
@@ -36,13 +36,13 @@ class DataBaseService {
                     Firebase.database.reference.child("products").child(id.toString()).child("id")
                         .setValue(id)
                     Firebase.database.reference.child("products").child(id.toString()).child("name")
-                        .setValue(name)
+                        .setValue(name.lowercase())
                     Firebase.database.reference.child("products").child(id.toString())
                         .child("price").setValue(price)
                     Firebase.database.reference.child("products").child(id.toString())
-                        .child("image").setValue(image)
+                        .child("image").setValue(image.lowercase())
                     Firebase.database.reference.child("products").child(id.toString())
-                        .child("category").setValue(category)
+                        .child("category").setValue(category.lowercase())
                 }
                 .addOnFailureListener {
                     // Handle any errors
@@ -51,16 +51,16 @@ class DataBaseService {
 
         fun getProductsByCategory(category: String): List<DataProduct> {
             val productsByCategory = mutableListOf<DataProduct>()
-            Firebase.database.reference.child("products").get().addOnSuccessListener {
+            Firebase.database.reference.child("products").get().addOnSuccessListener { it ->
                 val products = it.children
                 products.forEach {
                     if (it.child("category").value == category) {
                         val product = DataProduct(
                             it.child("id").value.toString().toInt(),
-                            it.child("name").value.toString(),
+                            it.child("name").value.toString().lowercase(),
                             it.child("price").value.toString().toInt(),
-                            it.child("image").value.toString(),
-                            it.child("category").value.toString()
+                            it.child("image").value.toString().lowercase(),
+                            it.child("category").value.toString().lowercase()
                         )
                         println("product: $product")
                         productsByCategory.add(product)
