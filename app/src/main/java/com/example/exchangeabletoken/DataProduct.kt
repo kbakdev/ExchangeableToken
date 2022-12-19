f600db1d6bbfe0a58f8fe6e18ce314fe9dc4aabe
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2022-12-19 17:13:39 +0100

    Firebase storage (#79)
    * feat: firebase support
    
    * feat: idea
    
    * feat: merge
    
    * feat: add the firebase authentication SDK
    
    * feat: added integration with firebase and sign out
    
    * feat: added login to firebase, and connection with login provider
    
    * feat: logIn and market
    
    * feat: market and login
    
    * feat: login
    
    * feat: bump up build versions
    
    * feat: Firebase Storage
    
    Co-authored-by: Kacper Bąk <kacper.bak@sonderfin.com>

--- app/src/main/java/com/example/exchangeabletoken/DataProduct.kt
+++ app/src/main/java/com/example/exchangeabletoken/DataProduct.kt
@@ -1,9 +1,13 @@
 package com.example.exchangeabletoken
 
+import android.graphics.Bitmap
+
 data class DataProduct(
     val id: Int,
     val name: String,
     val price: Int,
-    val image: String,
+    val image: Bitmap,
     val category: String
-)
+) {
+    val message: String? = null
+}
--- app/src/main/java/com/example/exchangeabletoken/FirebaseDatabase.kt
+++ app/src/main/java/com/example/exchangeabletoken/FirebaseDatabase.kt
@@ -1,17 +1,20 @@
 package com.example.exchangeabletoken
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.text.method.DialerKeyListener.CHARACTERS
+import java.net.URL
 import java.util.*
 
 class FirebaseDatabase {
     companion object {
-        fun getMarketData(): List<DataProduct> {
+        fun getMarketData(): List<DataProduct> {1
             return listOf(
                 DataProduct(
                     generateRandomId(),
                     generateRandomString(),
                     generateRandomPrice(),
-                    generateRandomUrlWithImage(),
+                    createRandomBitmap(),
                     generateRandomString()
                 ),
             )
@@ -37,15 +40,12 @@ class FirebaseDatabase {
             return random.nextInt(100)
         }
 
-        private fun generateRandomUrlWithImage(): String {
+        // create randomized mock bitmap
+        private fun createRandomBitmap(): Bitmap {
             val random = Random()
-            val randomUrl = StringBuilder()
-            val characters = CHARACTERS
-            while (randomUrl.length < 10) {
-                val index = random.nextInt(characters.size)
-                randomUrl.append(characters[index])
-            }
-            return "https://picsum.photos/200/300?random=$randomUrl".lowercase()
+            val width = random.nextInt(1000)
+            val height = random.nextInt(1000)
+            return Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         }
     }
 }
