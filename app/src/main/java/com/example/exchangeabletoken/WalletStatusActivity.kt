494c43065255c6acb536adf26645dc5dca98b284
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2023-01-29 18:48:43 +0100

    Ids (#90)
    * feat: firebase support
    
    * feat: idea
    
    * feat: merge
    
    * feat: add the firebase authentication SDK
    
    * feat: added integration with firebase and sign out
    
    * feat: added login to firebase, and connection with login provider
    
    * feat: logIn and market
    
    * feat: market and login
    
    * feat: login
    
    * feat: bump up build versions
    
    * fix: CI workflow
    
    * feat: ids
    
    * feat: ids
    
    * fix: xml design
    
    * fix: xml design
    
    * fix: views
    
    ---------
    
    Co-authored-by: Kacper Bąk <kacper.bak@sonderfin.com>

--- app/src/main/java/com/example/exchangeabletoken/WalletStatusActivity.kt
+++ app/src/main/java/com/example/exchangeabletoken/WalletStatusActivity.kt
@@ -10,7 +10,6 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 
 class WalletStatusActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallet_status)
@@ -22,16 +21,16 @@ class WalletStatusActivity : AppCompatActivity() {
         val database = Firebase.database
         val myRef = database.getReference("users")
         myRef.child(user?.uid.toString()).child("balance").get()
-            .addOnSuccessListener {
-                balance.text = "Balance: ${it.value}"
+            .addOnSuccessListener { it ->
+                "Balance: ${it.value}".also { balance.text = it }
             }
             .addOnFailureListener {
-                balance.text = "Balance: 0"
+                "Balance: 0".also { balance.text = it }
             }
         val output = findViewById<TextView>(R.id.output)
         // get user's output from database
-        FirebaseStorage.getInstance().reference.child("users/${user?.uid}/output").downloadUrl.addOnSuccessListener {
-            output.text = "Output: $it"
+        FirebaseStorage.getInstance().reference.child("users/${user?.uid}/output").downloadUrl.addOnSuccessListener { it ->
+            "Output: $it".also { output.text = it }
         }
     }
 }
