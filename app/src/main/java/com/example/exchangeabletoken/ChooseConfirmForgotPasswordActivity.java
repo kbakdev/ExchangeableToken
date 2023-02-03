028898934f5b976f9f490998e05d6699ac10116f
Kacper Bąk <kacperbak290@gmail.com>
2023-02-03 22:20:13 +0100

    fix: cleanup

--- app/src/main/java/com/example/exchangeabletoken/ChooseConfirmForgotPasswordActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.exchangeabletoken;
-
-import android.app.Activity;
-
-public class ChooseConfirmForgotPasswordActivity extends Activity {
-}
--- app/src/main/java/com/example/exchangeabletoken/resources.kt
+++
@@ -1,9 +0,0 @@
-package com.example.exchangeabletoken
-
-object resources {
-    const val BASE_URL = "https://api.exchangeratesapi.io/"
-    const val GET_RATES = "latest"
-    const val GET_RATES_BY_DATE = "{date}"
-    const val GET_RATES_BY_DATE_AND_CURRENCY = "{date}?base={currency}"
-    const val GET_RATES_BY_CURRENCY = "?base={currency}"
-}
